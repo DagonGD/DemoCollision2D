d4f926e55ec7533f823c6b2f47b8ce3f18c20a6a
antgit <iljinaa@gmail.com>
2016-03-23 22:47:27 +0200

    Initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody;
+
+    private const float InitialForce = 5;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _rigidBody = GetComponent<Rigidbody2D>();
+	    _rigidBody.AddForce(new Vector2(Random.Range(-InitialForce, InitialForce), Random.Range(-InitialForce, InitialForce)), ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        _rigidBody.AddForce(new Vector2(0,5), ForceMode2D.Impulse);
+    }
+}
